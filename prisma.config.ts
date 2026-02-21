fa5b67aedd8a901d279953c4c9b9192629a5b5a8
mishalaljuaid <mishalaljuaid@hotmail.com>
2026-02-21 14:26:19 +0300

    Fix Prisma config for production build

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,9 +5,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default defineConfig({
-    earlyAccess: true,
     schema: path.join(__dirname, 'prisma', 'schema.prisma'),
     datasource: {
-        url: process.env.DATABASE_URL,
+        url: process.env.DATABASE_URL || 'postgresql://dummy:dummy@localhost:5432/dummy',
     },
 });
